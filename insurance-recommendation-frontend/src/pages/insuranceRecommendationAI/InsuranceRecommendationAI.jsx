52d25790db38f443b29b1ce7db6b9bcf129ef539
Maryanne G <maryanneg@missionreadyhq.com>
2024-12-09 20:18:38 +1300

    set up basic setup following wireframe - text area, text box for user input and submit button

--- insurance-recommendation-frontend/src/pages/insuranceRecommendationAI/InsuranceRecommendationAI.jsx
+++ insurance-recommendation-frontend/src/pages/insuranceRecommendationAI/InsuranceRecommendationAI.jsx
@@ -1,7 +1,78 @@
 import BaseLayout from "../../layouts/baselayout/BaseLayout";
+import styles from "./InsuranceRecommendationAI.module.css";
 
 function InsuranceRecommendationAI() {
-  return <BaseLayout>Testing</BaseLayout>;
+  return (
+    <BaseLayout>
+      <div className={styles.mainContainer}>
+        <div className={styles.ChatBotContainer}>
+          <h1 className={styles.chatbotHeading}>
+            Chat with Tina! - Your AI Insurance Policy Advisor
+          </h1>
+          <div className={styles.chatDisplayArea}>
+            <div className={styles.AI_responseContainer}>
+              <div className={styles.scrollContainer}>
+                "Hi! I'm Tina, your AI Insurance Policy Advisor. How can I help
+                you today? Hi! I'm Tina, your AI Insurance Policy Advisor. How
+                can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today? Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today?Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today? Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today?Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today? Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today?Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today? Hi! I'm Tina, your AI Insurance Policy Advisor. How can I
+                help you today? Hi! I'm Tina, your AI Insurance Policy Advisor.
+                How can I help you today? Hi! I'm Tina, your AI Insurance Policy
+                Advisor. How can I help you today? Hi! I'm Tina, your AI
+                Insurance Policy Advisor. How can I help you today? Hi! I'm
+                Tina, your AI Insurance Policy Advisor. How can I help you
+                today?"
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.userInputContainer}>
+            <input type="text" className={styles.userInput} />
+            <input type="submit" value="Send" className={styles.submitButton} />
+          </div>
+        </div>
+      </div>
+    </BaseLayout>
+  );
 }
 
 export default InsuranceRecommendationAI;
